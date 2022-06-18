e193e58e55f0e39a42819434b3e89512703c7669
tuanmcwar <tuanntch1@gmail.com>
2022-06-18 18:00:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,577 @@
+// var button = document.querySelectorAll(".btn-active");
+// for (var i = 0; i < button.length; i++) {
+//   let index = i;
+//   if (i == 0) {
+//     button[i].classList.add("active");
+//   }
+//   button[i].addEventListener("click", function () {
+//     for (var j = 0; j < button.length; j++) {
+//       if (index != j) {
+//         button[j].classList.remove("active");
+//       } else {
+//         button[j].classList.add("active");
+//       }
+//     }
+//   });
+// }
+document.getElementById("bai1").focus();
+
+document.getElementById("bai1").onclick = function () {
+  var Bai1 = document.getElementById("content1");
+  Bai1.style.display = "block";
+
+  // var Bai1 = document.getElementById("bai1");
+  // Bai1.innerHTML = "📖Bài 1";
+
+  var Bai1 = document.getElementById("content2");
+  Bai1.style.display = "none";
+  var Bai1 = document.getElementById("content3");
+  Bai1.style.display = "none";
+  var Bai1 = document.getElementById("content4");
+  Bai1.style.display = "none";
+  var Bai1 = document.getElementById("content5");
+  Bai1.style.display = "none";
+};
+// function Bai1() {
+//     var Bai1 = document.getElementById("content1");
+//     Bai1.style.display = "block";
+// }
+document.getElementById("bai2").onclick = function () {
+  var Bai2 = document.getElementById("content2");
+  Bai2.style.display = "block";
+  // var Bai2 = document.getElementById("bai2");
+  // Bai2.innerHTML = "📖Bài 2";
+
+  var Bai2 = document.getElementById("content1");
+  Bai2.style.display = "none";
+  var Bai2 = document.getElementById("content3");
+  Bai2.style.display = "none";
+  var Bai2 = document.getElementById("content4");
+  Bai2.style.display = "none";
+  var Bai2 = document.getElementById("content5");
+  Bai2.style.display = "none";
+};
+document.getElementById("bai3").onclick = function () {
+  var Bai3 = document.getElementById("content3");
+  Bai3.style.display = "block";
+  // var Bai3 = document.getElementById("bai3");
+  // Bai3.innerHTML = "📖Bài 3";
+  var Bai3 = document.getElementById("content1");
+  Bai3.style.display = "none";
+  var Bai3 = document.getElementById("content2");
+  Bai3.style.display = "none";
+  var Bai3 = document.getElementById("content4");
+  Bai3.style.display = "none";
+  var Bai3 = document.getElementById("content5");
+  Bai3.style.display = "none";
+};
+document.getElementById("bai4").onclick = function () {
+  var Bai4 = document.getElementById("content4");
+  Bai4.style.display = "block";
+  // var Bai4 = document.getElementById("bai4");
+  // Bai4.innerHTML = "📖Bài 4";
+  var Bai4 = document.getElementById("content1");
+  Bai4.style.display = "none";
+  var Bai4 = document.getElementById("content2");
+  Bai4.style.display = "none";
+  var Bai4 = document.getElementById("content3");
+  Bai4.style.display = "none";
+  var Bai4 = document.getElementById("content5");
+  Bai4.style.display = "none";
+};
+document.getElementById("bai5").onclick = function () {
+  var Bai5 = document.getElementById("content5");
+  Bai5.style.display = "block";
+  // var Bai5 = document.getElementById("bai5");
+  // Bai5.innerHTML = "📖Bài 5";
+  var Bai5 = document.getElementById("content1");
+  Bai5.style.display = "none";
+  var Bai5 = document.getElementById("content2");
+  Bai5.style.display = "none";
+  var Bai5 = document.getElementById("content3");
+  Bai5.style.display = "none";
+  var Bai5 = document.getElementById("content4");
+  Bai5.style.display = "none";
+};
+// // bài 6
+document.getElementById("bai6").onclick = function () {
+  var Bai6 = document.getElementById("content6");
+  Bai6.style.display = "block";
+  // var Bai5 = document.getElementById("bai5");
+  // Bai5.innerHTML = "📖Bài 5";
+  var Bai6 = document.getElementById("content1");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content2");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content3");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content4");
+  Bai6.style.display = "none";
+  var Bai6 = document.getElementById("content5");
+  Bai6.style.display = "none";
+};
+// // bài 7
+document.getElementById("bai7").onclick = function () {
+  var Bai7 = document.getElementById("content7");
+  Bai7.style.display = "block";
+  // var Bai5 = document.getElementById("bai5");
+  // Bai5.innerHTML = "📖Bài 5";
+  var Bai7 = document.getElementById("content1");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content2");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content3");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content4");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content5");
+  Bai7.style.display = "none";
+  var Bai7 = document.getElementById("content6");
+  Bai7.style.display = "none";
+};
+// // bài 8
+document.getElementById("bai8").onclick = function () {
+  var Bai8 = document.getElementById("content8");
+  Bai8.style.display = "block";
+  // var Bai5 = document.getElementById("bai5");
+  // Bai5.innerHTML = "📖Bài 5";
+  var Bai8 = document.getElementById("content1");
+  Bai8.style.display = "none";
+  var Bai8 = document.getElementById("content2");
+  Bai8.style.display = "none";
+  var Bai8 = document.getElementById("content3");
+  Bai8.style.display = "none";
+  var Bai8 = document.getElementById("content4");
+  Bai8.style.display = "none";
+  var Bai8 = document.getElementById("content5");
+  Bai8.style.display = "none";
+  var Bai8 = document.getElementById("content6");
+  Bai8.style.display = "none";
+  var Bai8 = document.getElementById("content7");
+  Bai8.style.display = "none";
+};
+//  bài 1
+document.getElementById("btnB1").onclick = function () {
+  var Num1B1 = Number(document.getElementById("inputNum1B1").value);
+  var Num2B1 = Number(document.getElementById("inputNum2B1").value);
+  var Num3B1 = Number(document.getElementById("inputNum3B1").value);
+
+  if (Num1B1 > Num2B1) {
+    if (Num2B1 > Num3B1) {
+      document.getElementById("txtB1").innerHTML =
+        Num1B1 + "-" + Num2B1 + "-" + Num3B1;
+    } else {
+      document.getElementById("txtB1").innerHTML =
+        Num1B1 + "-" + Num3B1 + "-" + Num2B1;
+    }
+  }
+  if (Num2B1 > Num1B1) {
+    if (Num1B1 > Num3B1) {
+      document.getElementById("txtB1").innerHTML =
+        Num2B1 + "-" + Num1B1 + "-" + Num3B1;
+    } else {
+      document.getElementById("txtB1").innerHTML =
+        Num2B1 + "-" + Num3B1 + "-" + Num1B1;
+    }
+  }
+  if (Num3B1 > Num2B1) {
+    if (Num2B1 > Num1B1) {
+      document.getElementById("txtB1").innerHTML =
+        Num3B1 + "-" + Num2B1 + "-" + Num1B1;
+    } else {
+      document.getElementById("txtB1").innerHTML =
+        Num3B1 + "-" + Num1B1 + "-" + Num2B1;
+    }
+  }
+};
+// // bài 2
+document.getElementById("btnB2").onclick = function () {
+  var Num1B2 = document.getElementById("num1B2").value;
+
+  if (Num1B2 === "1") {
+    document.getElementById("txtB2").innerHTML = "Xin chào Người lạ ơi!";
+  } else if (Num1B2 === "2") {
+    document.getElementById("txtB2").innerHTML = "Xin chào Bố!";
+  } else if (Num1B2 === "3") {
+    document.getElementById("txtB2").innerHTML = "Xin chào Mẹ!";
+  } else if (Num1B2 === "4") {
+    document.getElementById("txtB2").innerHTML = "Xin chào Em Trai!";
+  } else {
+    document.getElementById("txtB2").innerHTML = "Xin chào Em Gái!";
+  }
+};
+// bài 3
+document.getElementById("btnB3").onclick = function () {
+  var Num1B3 = Number(document.getElementById("num1B3").value);
+  var Num2B3 = Number(document.getElementById("num2B3").value);
+  var Num3B3 = Number(document.getElementById("num3B3").value);
+
+  var count = 0;
+  if (Num1B3 % 2 === 0) {
+    count++;
+  }
+  if (Num2B3 % 2 === 0) {
+    count++;
+  }
+  if (Num3B3 % 2 === 0) {
+    count++;
+  }
+  document.getElementById("txtB3").innerHTML =
+    "Có " + count + " số chẳn" + " - " + (3 - count) + " số lẻ";
+};
+// //     bài 4
+document.getElementById("btnB4").onclick = function () {
+  var day1 = Number(document.getElementById("num1B4").value);
+  var day2 = Number(document.getElementById("num2B4").value);
+  var day3 = Number(document.getElementById("num3B4").value);
+  var huyen = 0;
+  var canh1 = 0;
+  var canh2 = 0;
+  if (day1 > day2 && day1 > day3) {
+    huyen = day1;
+    canh1 = day2;
+    canh2 = day3;
+  } else if (day2 > day1 && day2 > day3) {
+    huyen = day2;
+    canh1 = day1;
+    canh2 = day3;
+  } else if (day3 > day1 && day3 > day2) {
+    huyen = day3;
+    canh1 = day2;
+    canh2 = day1;
+  }
+  // console.log("huyen" + huyen);
+  // console.log("day1" + canh2);
+  // console.log("day2" + canh1);
+
+  if (day1 === day2 && day2 === day3 && day3 === day1) {
+    // if ((huyen = Math.sqrt()))
+    document.getElementById("txtB4").innerHTML = "Hình tam giác đều";
+  } else if (day1 === day2 || day2 === day3 || day3 === day1) {
+    document.getElementById("txtB4").innerHTML = "Hình tam giác cân";
+  } else if (huyen === Math.sqrt(canh1 * canh1 + canh2 * canh2)) {
+    document.getElementById("txtB4").innerHTML = "Hình tam giác vuông";
+  } else {
+    document.getElementById("txtB4").innerHTML = "Một loại tam giác nào đó";
+  }
+};
+// // bài 5
+// document.getElementById("btnB5").onclick = function () {
+//   var number = Number(document.getElementById("num1B5").value);
+//   var chuc = Math.floor(number / 10);
+//   var donvi = number % 10;
+//   var KQB5 = 0;
+//   KQB5 = chuc + donvi;
+//   document.getElementById("txtB5").innerHTML = KQB5;
+// };
+
+function Yesterday() {
+  var day5 = Number(document.getElementById("inputDay").value);
+  var month5 = Number(document.getElementById("inputMonth").value);
+  var year5 = Number(document.getElementById("inputYear").value);
+
+  if (
+    month5 === 12 ||
+    month5 === 3 ||
+    month5 === 5 ||
+    month5 === 7 ||
+    month5 === 8 ||
+    month5 === 10
+  ) {
+    if (day5 >= 2 && day5 <= 31) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + (day5 - 1) + "Tháng:" + month5 + "Năm:" + year5;
+    } else if (day5 === 1) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + 30 + "Tháng:" + (month5 - 1) + "Năm:" + year5;
+    } else {
+      document.getElementById("txtB5").innerHTML = "Ngày không đúng";
+    }
+  } else if (month5 === 4 || month5 === 6 || month5 === 9 || month5 === 11) {
+    if (day5 >= 2 && day5 <= 30) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + (day5 - 1) + "Tháng:" + month5 + "Năm:" + year5;
+    } else if (day5 === 1) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + 31 + "Tháng:" + (month5 - 1) + "Năm:" + year5;
+    } else {
+      document.getElementById("txtB5").innerHTML = "Ngày không đúng";
+    }
+  } else if (month5 === 1) {
+    if (day5 >= 2 && day5 <= 31) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + (day5 - 1) + "Tháng:" + month5 + "Năm:" + year5;
+    } else if (day5 === 1) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + 31 + "Tháng:" + 12 + "Năm:" + (year5 - 1);
+    } else {
+      document.getElementById("txtB5").innerHTML = "Ngày không đúng";
+    }
+  } else if (month5 === 2) {
+    if (day5 >= 2 && day5 <= 28) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + (day5 - 1) + "Tháng:" + month5 + "Năm:" + year5;
+    } else if (day5 === 1) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + 31 + "Tháng:" + (month5 - 1) + "Năm:" + year5;
+    } else {
+      document.getElementById("txtB5").innerHTML = "Ngày không đúng";
+    }
+  } else if (month5 > 12) {
+    document.getElementById("txtB5").innerHTML = "Tháng không đúng";
+  }
+}
+function Tomorrow() {
+  var day5 = Number(document.getElementById("inputDay").value);
+  var month5 = Number(document.getElementById("inputMonth").value);
+  var year5 = Number(document.getElementById("inputYear").value);
+
+  if (
+    month5 === 1 ||
+    month5 === 3 ||
+    month5 === 5 ||
+    month5 === 7 ||
+    month5 === 8 ||
+    month5 === 10
+  ) {
+    if (day5 >= 1 && day5 <= 30) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + (day5 + 1) + "Tháng:" + month5 + "Năm:" + year5;
+    } else if (day5 === 31) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + 1 + "Tháng:" + (month5 + 1) + "Năm:" + year5;
+    } else {
+      document.getElementById("txtB5").innerHTML = "Ngày không đúng";
+    }
+  } else if (month5 === 4 || month5 === 6 || month5 === 9 || month5 === 11) {
+    if (day5 >= 1 && day5 <= 29) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + (day5 + 1) + "Tháng:" + month5 + "Năm:" + year5;
+    } else if (day5 === 30) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + 1 + "Tháng:" + (month5 + 1) + "Năm:" + year5;
+    } else {
+      document.getElementById("txtB5").innerHTML = "Ngày không đúng";
+    }
+  } else if (month5 === 12) {
+    if (day5 >= 1 && day5 <= 30) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + (day5 + 1) + "Tháng:" + month5 + "Năm:" + year5;
+    } else if (day5 === 31) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + 1 + "Tháng:" + 1 + "Năm:" + (year5 + 1);
+    } else {
+      document.getElementById("txtB5").innerHTML = "Ngày không đúng";
+    }
+  } else if (month5 === 2) {
+    if (day5 >= 1 && day5 <= 27) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + (day5 + 1) + "Tháng:" + month5 + "Năm:" + year5;
+    } else if (day5 === 28) {
+      document.getElementById("txtB5").innerHTML =
+        "Ngày:" + 1 + "Tháng:" + (month5 + 1) + "Năm:" + year5;
+    } else {
+      document.getElementById("txtB5").innerHTML = "Ngày không đúng";
+    }
+  } else if (month5 > 12) {
+    document.getElementById("txtB5").innerHTML = "Tháng không đúng";
+  }
+}
+document.getElementById("btnYesterday").onclick = function () {
+  Yesterday();
+};
+document.getElementById("btnTomorrow").onclick = function () {
+  Tomorrow();
+};
+
+function TinhNgay() {
+  var thangBai6 = Number(document.getElementById("num1B6").value);
+  var namBai6 = Number(document.getElementById("num2B6").value);
+  var soNgay = 0;
+  if (
+    thangBai6 === 1 ||
+    thangBai6 === 3 ||
+    thangBai6 === 5 ||
+    thangBai6 === 7 ||
+    thangBai6 === 8 ||
+    thangBai6 === 10 ||
+    thangBai6 === 12
+  ) {
+    soNgay = "Có 31 ngày";
+  } else if (
+    thangBai6 === 4 ||
+    thangBai6 === 6 ||
+    thangBai6 === 9 ||
+    thangBai6 === 11
+  ) {
+    soNgay = "Có 30 ngày";
+  } else if (thangBai6 === 2) {
+    if (namBai6 % 400 === 0) {
+      soNgay = "Có 29 ngày";
+    } else {
+      soNgay = "Có 28 ngày";
+    }
+  }
+  document.getElementById("txtB6").innerHTML =
+    "Tháng:" + thangBai6 + " Năm:" + namBai6 + " " + soNgay;
+}
+document.getElementById("btnB6").onclick = function () {
+  TinhNgay();
+};
+
+function Docso() {
+  var soBai7txt = document.getElementById("num1B7").value;
+
+  var soBai7 = Number(document.getElementById("num1B7").value);
+  var hangChuc = 0;
+  var hangTram = 0;
+  var hangDonvi = 0;
+  hangTram = Math.floor(soBai7 / 100);
+  hangChuc = Math.floor(soBai7 / 10) % 10;
+  hangDonvi = soBai7 % 10;
+  if (soBai7txt.length === 1) {
+    document.getElementById("txtB7").innerHTML = readNumber(hangDonvi);
+  } else if (soBai7txt.length === 2) {
+    document.getElementById("txtB7").innerHTML =
+      readNumber(hangChuc) + " " + " Mươi " + readNumber(hangDonvi);
+    if (hangDonvi === 0) {
+      document.getElementById("txtB7").innerHTML =
+        readNumber(hangChuc) + " Mươi";
+    }
+  } else if (soBai7txt.length === 3) {
+    document.getElementById("txtB7").innerHTML =
+      readNumber(hangTram) +
+      " " +
+      " Trăm " +
+      readNumber(hangChuc) +
+      " " +
+      " Mươi " +
+      readNumber(hangDonvi);
+    if (hangChuc === 0) {
+      document.getElementById("txtB7").innerHTML =
+        readNumber(hangTram) +
+        " " +
+        " Trăm " +
+        readNumber(hangChuc) +
+        " " +
+        readNumber(hangDonvi);
+    }
+  }
+}
+//   if (hangTram === 0 && hangChuc !== 0) {
+//     document.getElementById("txtB7").innerHTML =
+//       readNumber(hangChuc) + " " + " Mươi " + readNumber(hangDonvi);
+//   } else if (hangChuc === 0) {
+
+//     document.getElementById("txtB7").innerHTML = readNumber(hangDonvi);
+//   } else if (hangDonvi === 0) {
+//     document.getElementById("txtB7").innerHTML =
+//       readNumber(hangTram) +
+//       " " +
+//       " Trăm " +
+//       readNumber(hangChuc) +
+//       " " +
+//       " Mươi ";
+//   } else {
+//     document.getElementById("txtB7").innerHTML =
+//       readNumber(hangTram) +
+//       " " +
+//       " Trăm " +
+//       readNumber(hangChuc) +
+//       " " +
+//       " Mươi " +
+//       readNumber(hangDonvi);
+//   }
+// }
+
+function readNumber(number) {
+  var textNum = "";
+  switch (number) {
+    case 0:
+      return (textNum = "Lẻ");
+      break;
+    case 1:
+      return (textNum = "Một");
+      break;
+    case 2:
+      return (textNum = "Hai");
+      break;
+    case 3:
+      return (textNum = "Ba");
+      break;
+    case 4:
+      return (textNum = "Bốn");
+      break;
+    case 5:
+      return (textNum = "Năm");
+      break;
+    case 6:
+      return (textNum = "Sáu");
+      break;
+    case 7:
+      return (textNum = "Bảy");
+      break;
+    case 8:
+      return (textNum = "Tám");
+      break;
+    case 9:
+      return (textNum = "Chín");
+      break;
+  }
+}
+document.getElementById("btnB7").onclick = function () {
+  Docso();
+};
+
+function DoanDuong(ten, x, y, xTruong, yTruong) {
+  var xSv = Number(document.getElementById(x).value);
+  var ySv = Number(document.getElementById(y).value);
+  var xtruong = Number(document.getElementById(xTruong).value);
+  var ytruong = Number(document.getElementById(yTruong).value);
+
+  duongdi = 0;
+
+  duongdi = Math.pow(xtruong - xSv, 2) + Math.pow(ytruong - ySv, 2);
+  return Math.sqrt(duongdi);
+}
+
+document.getElementById("btnSearch").onclick = function () {
+  var tenSv1 = document.getElementById("inputName1").value;
+  var tenSv2 = document.getElementById("inputName2").value;
+  var tenSv3 = document.getElementById("inputName3").value;
+
+  var sinhvien1 = DoanDuong(
+    "inputName1",
+    "inputX1",
+    "inputY1",
+    "inputX4",
+    "inputY4"
+  );
+  var sinhvien2 = DoanDuong(
+    "inputName2",
+    "inputX2",
+    "inputY2",
+    "inputX4",
+    "inputY4"
+  );
+  var sinhvien3 = DoanDuong(
+    "inputName3",
+    "inputX3",
+    "inputY3",
+    "inputX4",
+    "inputY4"
+  );
+
+  var SvXanhat = Math.max(sinhvien1, sinhvien2, sinhvien3);
+
+  if (SvXanhat === sinhvien1) {
+    document.getElementById("txtB8").innerHTML =
+      "Sinh viên xa trường nhất: " + tenSv1;
+  } else if (SvXanhat === sinhvien2) {
+    document.getElementById("txtB8").innerHTML =
+      "Sinh viên xa trường nhất: " + tenSv2;
+  } else if (SvXanhat === sinhvien3) {
+    document.getElementById("txtB8").innerHTML =
+      "Sinh viên xa trường nhất: " + tenSv3;
+  }
+};
